a45a9bad51b59eec4f78d1b21bd65d86a2f0468f
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-08-10 16:45:12 -0400

    finished the lab

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,34 @@
+require "pry"
+
+class CashRegister
+
+    attr_accessor :total, :discount, :items, :previous_total
+
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+
+    def add_item(title, price, quantity = 1)
+        @previous_total = price * quantity
+        @total += @previous_total
+        quantity.times do
+        @items << title
+        end
+    end
+
+    def apply_discount
+       if @discount != 0
+        @total -= (@total * @discount)/ 100
+        "After the discount, the total comes to $#{@total}."
+       else 
+        "There is no discount to apply."
+       end
+    end
+
+    def void_last_transaction
+        @total -= @previous_total
+    end
+end
 
